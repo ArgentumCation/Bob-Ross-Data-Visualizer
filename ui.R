3728013b477e92d1bbcb5dbc1b7465f4fa9ae6eb
Ajay Kristipati <ajayk111@users.noreply.github.com>
2019-06-03 14:32:07 -0700

    added ui layout

--- ui.R
+++ ui.R
@@ -1,3 +1,7 @@
 library('shiny')
 
-ui <- fluidPage()
+ui <- fluidPage(titlePanel('Ott Toomet\'s Fan Club Presents Bob Ross'), tags$b(tags$i(tags$u('End my suffering'))),
+                sidebarLayout(sidebarPanel(
+                  'Sidebar'
+                ),mainPanel(
+                  'Main')))
