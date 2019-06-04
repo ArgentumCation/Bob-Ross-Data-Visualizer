b7f6e269598b2852ef0ae42296dd5a534ffdb6db
Gunhyung-Cho <ghcho@uw.edu>
2019-06-04 15:34:06 -0700

    table not showing

--- ui.R
+++ ui.R
@@ -6,11 +6,13 @@ features <- str_to_title( gsub('_',' ',colnames(as.data.frame(raw_data))))
 features <- features[4:(length(features) - 1)]
 
 ui <- fluidPage(
-    titlePanel('Ott Toomet\'s Fan Club Presents Bob Ross'), tags$b(tags$i(tags$u('End my suffering'))),
-    sidebarLayout(
-      sidebarPanel(
-        checkboxGroupInput("checkGroup", label = h3("Checkbox group"), choices = features, selected = 1)
-      ),
-    mainPanel('Main')
+    titlePanel('Ott Toomet\'s Fan Club Presents Bob Ross'), 
+    tags$b(tags$i(tags$u('End my suffering'))),
+    sidebarPanel(
+      checkboxGroupInput("checkGroup", label = h3("Checkbox group"), choices = features)
+    ),
+    mainPanel(
+      #output is a table for debugging purposes
+      dataTableOutput(outputId = "plot")
     )
   )
