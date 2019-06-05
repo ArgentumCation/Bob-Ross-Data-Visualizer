4a74b5663e7121cfc591e777020ce51056158515
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 02:18:11 -0700

    fixed format

--- server.R
+++ server.R
@@ -7,16 +7,9 @@ source("joint_freq_server.R")
 source("plot2server.R")
 source("plot3server.R")
 
-
-
-
 server <- function(input, output) {
   # # Call Plotting Functions Here
-  #   #output is a table for debugging purposes
-  #   reactive(print(input$checkGroup))
-  #   output$plot <- renderDataTable(
-  #     #select(raw_data, input$checkGroup)
-  #  )
+  
   output$plot1 <- renderPlot(joint_freq(input$jointCheck))
   output$plot2 <- renderDataTable(getEpisode(input$checkGroup))
   output$plot3 <- renderPlot(getCounts(input$checkGroup2))
