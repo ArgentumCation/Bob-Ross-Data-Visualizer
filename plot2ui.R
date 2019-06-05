866f002a9646dd625594b4eaebecc6895d4c6af8
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 02:17:48 -0700

    fixed format

--- plot2ui.R
+++ plot2ui.R
@@ -1,8 +1,3 @@
-raw_data <- read.csv("cleaned_data.csv", stringsAsFactors = FALSE)
-
-features <- gsub('_', ' ', colnames(as.data.frame(raw_data)))
-features <- features[4:(length(features) - 1)]
-
 plot2ui <- tabPanel(
   "Plot 2",
   sidebarPanel(
@@ -13,9 +8,8 @@ plot2ui <- tabPanel(
     )
   ),
   mainPanel(
-    #output is a table for debugging purposes
-    shiny::dataTableOutput(outputId = "plot2"),
-    shiny::hr(),
+    dataTableOutput(outputId = "plot2"),
+    hr(),
     "Episodes can be found at:",
     a(href = "https://www.youtube.com/channel/UCxcnsr1R5Ge_fbTu5ajt8DQ", 'Youtube'),
     a(href = "https://www.twitch.tv/bobross", "Twitch"),
