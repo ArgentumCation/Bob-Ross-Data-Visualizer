4722f1dc865ae99709ed3007e8ad22206e48c39f
zoemoser <zmoser@uw.edu>
2019-06-05 12:35:01 -0700

    finished works cited

--- ui.R
+++ ui.R
@@ -17,5 +17,11 @@ ui <- navbarPage(theme=shinytheme('flatly'),
              img(src = "Bob_Ross_2.jpg")),
     plot1ui, #joint frequency plot
     plot2ui, #episode search with tag
-    plot3ui #compare bob and guests
+    plot3ui, #compare bob and guests
+    tabPanel("Works Cited", 
+             p("Bob Ross dataset:", 
+              a("https://github.com/fivethirtyeight/data/blob/master/bob-ross/elements-by-episode.csv"), 
+               p("collected and created by Walt Hickey, and accessed from FiveThirtyEight’s public Github repository."))),
+            p("Bob Ross Picture:", 
+              a("https://www.businessinsider.com.au/amazon-twitch-creative-bob-ross-joy-of-painting-marathon-2015-10"))
 )
