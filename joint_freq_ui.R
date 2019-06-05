132ef171c41eb393c15863d7c72455c938f2fabd
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 03:39:12 -0700

    Added descriptions

--- joint_freq_ui.R
+++ joint_freq_ui.R
@@ -1,14 +1,16 @@
 plot1ui <- tabPanel(
-  "Plot 1",
+  "Joint Frequency of Features",
   sidebarPanel(
     checkboxGroupInput(
       "jointCheck",
       label = h3("Features"),
       choices = features
-    )
+    ),
+    p(em("Select features to see the joint frequency plot for selected features")),
+    p(strong("Joint Frequency:"), "How many times a combination of multiple conditions happens together")
   ),
   mainPanel(
-    plotOutput(outputId = "plot1"),
+    plotOutput(outputId = "plot1", height = "600px"),
     hr(),
     "Episodes can be found at:",
     a(href = "https://www.youtube.com/channel/UCxcnsr1R5Ge_fbTu5ajt8DQ", 'Youtube'),
