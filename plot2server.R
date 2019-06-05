f1c661f2625d1c8ab2c02c6f2c2862a9326a0c5c
Ajay Kristipati <ajayk111@users.noreply.github.com>
2019-06-04 23:02:41 -0700

    cleaned code

--- plot2server.R
+++ plot2server.R
@@ -1,9 +1,13 @@
-raw_data <- read.csv("cleaned_data.csv") # Original DataFrame from csv file
+raw_data <-
+  read.csv("cleaned_data.csv") # Original DataFrame from csv file
 
-for (col in 4:ncol(raw_data)){
-  for(row in 1:nrow(raw_data)){
-    raw_data[row,col] = if (raw_data[row,col] == 1) "Yes" else "No"
-  } 
+for (col in 4:ncol(raw_data)) {
+  for (row in 1:nrow(raw_data)) {
+    raw_data[row, col] = if (raw_data[row, col] == 1)
+      "Yes"
+    else
+      "No"
+  }
 }
 
 getEpisode <- function (features) {
@@ -11,14 +15,16 @@ getEpisode <- function (features) {
   
   for (val in features) {
     #Get data frames where all elements have 1 in the right column
-    temp <- list(raw_data[raw_data[val] == "Yes", ])
+    temp <- list(raw_data[raw_data[val] == "Yes",])
     #add that to our list
     filteredList <- c(filteredList , temp)
   }
   # merge the list of data frames into one (Set AND)
-  result <- filteredList %>% Reduce(function(d1, d2) merge(d1, d2), .)
-  # Get Relevant Columns  
-  result <- result[c(2,3)]
+  result <-
+    filteredList %>% Reduce(function(d1, d2)
+      merge(d1, d2), .)
+  # Get Relevant Columns
+  result <- result[c(2, 3)]
   # Make Pretty
   result$TITLE <- str_to_title(result$TITLE)
   return (result)
--- server.R
+++ server.R
@@ -2,27 +2,27 @@
 # Make sure "elements-by-episode.csv" is inside your working directory
 
 # Loads the required libraries.
-# Feel free to add more libraries to the "libs" list if necessary 
+# Feel free to add more libraries to the "libs" list if necessary
 source("plot2server.R")
 
-libs = c("ggplot2", "dplyr", "shiny","stringr")
+libs = c("ggplot2", "dplyr", "shiny", "stringr")
 
-for (i in libs){
-  if( !is.element(i, .packages(all.available = TRUE)) ) {
+for (i in libs) {
+  if (!is.element(i, .packages(all.available = TRUE))) {
     install.packages(i)
   }
-  library(i,character.only = TRUE)
+  library(i, character.only = TRUE)
 }
 
 
 
 
-server <- function(input, output){
-# # Call Plotting Functions Here
-#   #output is a table for debugging purposes
-#   reactive(print(input$checkGroup))
-#   output$plot <- renderDataTable(
-#     #select(raw_data, input$checkGroup)
-#  )
+server <- function(input, output) {
+  # # Call Plotting Functions Here
+  #   #output is a table for debugging purposes
+  #   reactive(print(input$checkGroup))
+  #   output$plot <- renderDataTable(
+  #     #select(raw_data, input$checkGroup)
+  #  )
   output$plot2 <- renderDataTable(getEpisode(input$checkGroup))
 }
