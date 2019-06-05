e4bc713fae8aced8c93bcb9767974764e2317590
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 03:38:33 -0700

    shows empty graph for no matching episodes

--- joint_freq_server.R
+++ joint_freq_server.R
@@ -1,45 +1,45 @@
-raw_data <- read.csv("cleaned_data.csv")
-
 joint_freq <- function(features) {
   
-  if(length(features) < 1) {
-    return("No data to display :(")
-  }
-  
   subset <- raw_data
-  for (elem in features) {
-    subset <- filter(subset, UQ(as.name(elem)) == 1)
-  }
-  subset <- select(subset, -features)
   
-  if(nrow(subset) < 1) {
-    return("No data to display :(")
+  # if features are selected, retrieve the data for selected features
+  # if no features are selected, use the original data
+  if(length(features) > 0) {
+    for (elem in features) {
+      subset <- filter(subset, UQ(as.name(elem)) == 1)
+    }
+    subset <- select(subset, -features)
   }
   
   #empty dataframe with 1 column and number of remaining columns (excluding user-selected columns)
   freq_data <- data.frame(matrix(NA, nrow = ncol(select(subset, -c("X", "EPISODE", "TITLE", "GUEST"))), ncol = 1))
   
+  # if there are episodes with selected features, store the frequency of each features into freq column of freq_data
+  # if there are no episodes with selected features, freq column of freq_data only stores 0
   freq_data <- freq_data %>%
     # assign the column names (except the first three) to "tags" column
     mutate(tags = colnames(select(subset, -c("X", "EPISODE", "TITLE", "GUEST")))) %>%
     mutate(freq = 0) %>%
     select(tags, freq)
   
-  # counts the frequency of each features and store it into freq_data dataframe
-  i <-  1
-  for(elem in freq_data$tags) {
-    count <- sum(subset %>%
-                   select(!!elem))
-    freq_data[i, 2] = count
-    i <- i + 1
+  if(nrow(subset) > 0) {
+    # counts the frequency of each features and store it into freq_data dataframe
+    i <-  1
+    for(elem in freq_data$tags) {
+      count <- sum(select(subset, !!elem))
+      freq_data[i, 2] = count
+      i <- i + 1
+    }
   }
   
   output <- ggplot(freq_data, aes(x=tags, y=freq, fill=tags)) +
     geom_bar(stat = "identity") +
     xlab("Features") +
     ylab("Number of Episodes") +
+    scale_y_continuous(breaks = function(x) unique(floor(pretty(seq(0, (max(x) + 1) * 1.1))))) +
     coord_flip() +
     theme(panel.background = element_rect(fill = "white"),
+          text = element_text(size=15),
           panel.grid.major.x = element_line(size = 0.5, linetype = "solid", colour = "grey95"),
           panel.grid.major.y = element_blank(),
           panel.grid.minor = element_blank()) +
