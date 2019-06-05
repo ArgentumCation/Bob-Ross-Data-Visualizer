45e491247cfa0b6f2454f11e238d312b7c9da957
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 02:18:15 -0700

    fixed format

--- ui.R
+++ ui.R
@@ -4,16 +4,18 @@ library(shinythemes)
 source("joint_freq_ui.R")
 source("plot2ui.R")
 source("plot3ui.R")
+
+raw_data <- read.csv("cleaned_data.csv", stringsAsFactors = FALSE)
+
+features <- gsub('_', ' ', colnames(as.data.frame(raw_data)))
+features <- features[4:(length(features) - 1)]
+
 ui <- navbarPage(theme=shinytheme('flatly'),
   'Ott Toomet\'s Fan Club Presents: Bob Ross',
-  
-  
-  
+
     tabPanel("Background",
              img(src = "Bob_Ross_2.jpg")),
-    plot1ui,
-    plot2ui,
-    plot3ui
-
-  
+    plot1ui, #joint frequency plot
+    plot2ui, #episode search with tag
+    plot3ui #compare bob and guests
 )
