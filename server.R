638d82505fc2faccc41a888416b3d1e76b8c8f40
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 03:37:35 -0700

    fixed format

--- server.R
+++ server.R
@@ -1,11 +1,19 @@
-library("ggplot2")
-library("dplyr")
-library("shiny")
-library ("stringr")
+# add any libraries we need
+libs <- c("ggplot2", "dplyr", "shiny", "stringr")
+
+# Loads libs, installs if not already installed
+for (i in libs) {
+  if (!is.element(i, .packages(all.available = TRUE))) {
+    install.packages(i)
+  }
+  library(i, character.only = TRUE)
+}
 
 source("joint_freq_server.R")
 source("plot2server.R")
 source("plot3server.R")
+
+raw_data <- read.csv("cleaned_data.csv")
 
 server <- function(input, output) {
   # # Call Plotting Functions Here
