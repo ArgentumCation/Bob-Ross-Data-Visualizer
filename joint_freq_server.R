e98ef7b3a584284e860c8a02ff3a2f68951b7dd8
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 02:11:16 -0700

    now returns the plot

--- joint_freq_server.R
+++ joint_freq_server.R
@@ -2,11 +2,19 @@ raw_data <- read.csv("cleaned_data.csv")
 
 joint_freq <- function(features) {
   
+  if(length(features) < 1) {
+    return("No data to display :(")
+  }
+  
   subset <- raw_data
   for (elem in features) {
     subset <- filter(subset, UQ(as.name(elem)) == 1)
   }
   subset <- select(subset, -features)
+  
+  if(nrow(subset) < 1) {
+    return("No data to display :(")
+  }
   
   #empty dataframe with 1 column and number of remaining columns (excluding user-selected columns)
   freq_data <- data.frame(matrix(NA, nrow = ncol(select(subset, -c("X", "EPISODE", "TITLE", "GUEST"))), ncol = 1))
@@ -26,5 +34,15 @@ joint_freq <- function(features) {
     i <- i + 1
   }
   
-  return(freq_data)
+  output <- ggplot(freq_data, aes(x=tags, y=freq, fill=tags)) +
+    geom_bar(stat = "identity") +
+    xlab("Features") +
+    ylab("Number of Episodes") +
+    coord_flip() +
+    theme(panel.background = element_rect(fill = "white"),
+          panel.grid.major.x = element_line(size = 0.5, linetype = "solid", colour = "grey95"),
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor = element_blank()) +
+    theme(legend.position="none")
+  return(output)
 }
