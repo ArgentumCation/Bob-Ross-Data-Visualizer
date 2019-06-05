fb166890253f5d7c19b640f60e3b1649840fa0a5
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 02:10:25 -0700

    initial plot 1 UI

---
+++ joint_freq_ui.R
@@ -0,0 +1,19 @@
+plot1ui <- tabPanel(
+  "Plot 1",
+  sidebarPanel(
+    checkboxGroupInput(
+      "jointCheck",
+      label = h3("Features"),
+      choices = features
+    )
+  ),
+  mainPanel(
+    #output is a table for debugging purposes
+    plotOutput(outputId = "plot1"),
+    hr(),
+    "Episodes can be found at:",
+    a(href = "https://www.youtube.com/channel/UCxcnsr1R5Ge_fbTu5ajt8DQ", 'Youtube'),
+    a(href = "https://www.twitch.tv/bobross", "Twitch"),
+    a(href = "https://www.netflix.com/", "Netflix")
+  )
+)
