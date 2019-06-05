27c041a1b6a9ed1a415164632a6d94a6a7b114b9
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 02:17:56 -0700

    fixed format

--- plot3ui.R
+++ plot3ui.R
@@ -1,8 +1,3 @@
-raw_data <- read.csv("cleaned_data.csv", stringsAsFactors = FALSE)
-
-features <- gsub('_', ' ', colnames(as.data.frame(raw_data)))
-features <- features[4:(length(features) - 1)]
-
 plot3ui <- tabPanel(
   "Plot 3",
   sidebarPanel(
@@ -13,9 +8,8 @@ plot3ui <- tabPanel(
     )
   ),
   mainPanel(
-    #output is a table for debugging purposes
-    shiny::plotOutput(outputId = "plot3"),
-    shiny::hr(),
+    plotOutput(outputId = "plot3"),
+    hr(),
     "Episodes can be found at:",
     a(href = "https://www.youtube.com/channel/UCxcnsr1R5Ge_fbTu5ajt8DQ", 'Youtube'),
     a(href = "https://www.twitch.tv/bobross", "Twitch"),
