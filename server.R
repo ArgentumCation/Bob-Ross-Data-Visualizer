6497930b9ef5ba3903b3d0d94b05e2fa803aeb01
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 18:25:52 -0700

    added select type buttons

--- server.R
+++ server.R
@@ -10,8 +10,20 @@ source("plot2server.R")
 source("plot3server.R")
 
 raw_data <- read.csv("cleaned_data.csv")
+features <- gsub('_', ' ', colnames(as.data.frame(guest_data)))
+features <- features[4:(length(features) - 1)]
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  observe({
+    if (input$select_type == "All") {
+      updateCheckboxGroupInput(session=session, inputId="checkGroup2", choices=features, selected=features)
+    }
+    else if (input$select_type == "Manual Select") {
+      updateCheckboxGroupInput(session=session, inputId="checkGroup2", choices=features, selected=NULL)
+    }
+  })
+  
   # # Call Plotting Functions Here
   
   output$plot1 <- renderPlot(joint_freq(input$jointCheck))
