a8b8b3bdcaec70372b2cc06aa57c41a781e9ecb9
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 18:25:21 -0700

    changed to percentage plot

--- plot3server.R
+++ plot3server.R
@@ -1,7 +1,23 @@
 raw_data <-
   read.csv("cleaned_data.csv") # Original DataFrame from csv file
 
+empty_plot <- ggplot(data.frame()) +
+  geom_bar(stat = "identity", position = "dodge") + 
+  theme(legend.position="none",
+        panel.background = element_rect(fill = "white"),
+        panel.grid.major.y = element_line(size = 0.5, linetype = "solid", colour = "grey95"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor = element_blank()) + 
+  ggtitle("Guest Work") +
+  ylim(0,100) +
+  ylab("Percent of Apperance (%)") +
+  xlab("Features")
+
 getGuestCounts <- function (features) {
+  if (length(features) < 1) {
+    return(empty_plot + ggtitle("Guest Work"))
+  }
+  
   guest_data <- raw_data[raw_data$GUEST == 1,]
   filteredList = vector()
   
@@ -16,14 +32,18 @@ getGuestCounts <- function (features) {
     count <- c(count, nrow(val))
   }
   #print(nrow(features))
-  if (length(features) < 1) {
-    plot <- "No data to display :("
-  }
-  else {
-    names <- data.frame("Features" = features, "Counts" = count)
-    plot <- ggplot(data = names, aes(x = Features, y = Counts, fill = Features)) + geom_bar(stat =
-                                                                                              "identity", position = "dodge") + theme(legend.position="none") + ggtitle("Guest Work")
-  }
+  names <- data.frame("Features" = features, "Percent" = count / nrow(guest_data) * 100)
+  plot <- ggplot(data = names, aes(x = Features, y = Percent, fill = Features)) + 
+    geom_bar(stat = "identity") + 
+    theme(legend.position="none",
+          panel.background = element_rect(fill = "white"),
+          panel.grid.major.y = element_line(size = 0.5, linetype = "solid", colour = "grey95"),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor = element_blank()) + 
+    ggtitle("Guest Work") +
+    ylim(0,100) +
+    ylab("Percent of Apperance (%)") +
+    xlab("Features")
   
   return(plot)
 }
@@ -32,6 +52,10 @@ getGuestCounts <- function (features) {
 #getCounts(features)
 
 getBobCounts <- function (features) {
+  if (length(features) < 1) {
+    return(empty_plot + ggtitle("Bob's Work"))
+  }
+  
   guest_data <- raw_data[raw_data$GUEST == 0,]
   filteredList = vector()
   
@@ -46,14 +70,19 @@ getBobCounts <- function (features) {
     count <- c(count, nrow(val))
   }
   #print(nrow(features))
-  if (length(features) < 1) {
-    plot <- "No data to display :("
-  }
-  else {
-    names <- data.frame("Features" = features, "Counts" = count)
-    plot <- ggplot(data = names, aes(x = Features, y = Counts, fill = Features)) + geom_bar(stat =
-                                                                                              "identity", position = "dodge") + theme(legend.position="none") + ggtitle("Bob's Work")
-  }
-  
+
+  names <- data.frame("Features" = features, "Percent" = count / nrow(guest_data) * 100)
+  plot <- ggplot(data = names, aes(x = Features, y = Percent, fill = Features)) + 
+    geom_bar(stat = "identity") + 
+    theme(legend.position="none",
+          panel.background = element_rect(fill = "white"),
+          panel.grid.major.y = element_line(size = 0.5, linetype = "solid", colour = "grey95"),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor = element_blank()) + 
+    ggtitle("Bob's Work") +
+    ylim(0,100) +
+    ylab("Percent of Apperance (%)") +
+    xlab("Features")
+
   return(plot)
 }
