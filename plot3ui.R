e73d9aaa64410bba3d6e5ef14343f0da5e201665
Gunhyung-Cho <ghcho@uw.edu>
2019-06-05 18:25:47 -0700

    added select type buttons

--- plot3ui.R
+++ plot3ui.R
@@ -6,12 +6,20 @@ features <- features[4:(length(features) - 1)]
 plot3ui <- tabPanel(
   "Ross: Guest vs. Solo",
   sidebarPanel(
-    checkboxGroupInput(
-      "checkGroup2",
-      label = h3("Features"),
-      choices = features
-    )
-  
+    conditionalPanel(
+      condition = "input.select_type != 'All'",
+        checkboxGroupInput(
+        "checkGroup2",
+        label = h3("Features"),
+        choices = features
+      )
+    ),
+    radioButtons(
+      inputId="select_type",
+      label="Feature Selection Type:",
+      choices=list("All", "Manual Select"),
+      selected="All"),
+    p(em("Select features to compare Bob and his guests"))
   ),
   mainPanel(
     plotOutput(outputId = "plot3Guest"),
